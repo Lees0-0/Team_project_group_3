270ec3ba40c608da7671de46b6c5397c676fb257
yeremizinak <eremizinaekaterina@gmail.com>
2023-08-29 00:49:55 +0300

    fixed

--- src/main/java/org/group_3/Main.java
+++ src/main/java/org/group_3/Main.java
@@ -1,5 +1,7 @@
 package org.group_3;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> HelloWindow.createHelloWindow());
--- src/main/java/org/group_3/ScoreWindow.java
+++ src/main/java/org/group_3/ScoreWindow.java
@@ -2,6 +2,8 @@ package org.group_3;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
 
 public class ScoreWindow {
     public static void createScoreWindow() {
@@ -13,6 +15,15 @@ public class ScoreWindow {
         int screenHeight = screenSize.height;
 
         JFrame frame = new JFrame("Рахунок гри");
+
+        try {
+            URL iconURL = new URL("https://media.istockphoto.com/id/1486992237/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%BF%D0%BB%D0%B0%D0%B2%D0%B0%D1%8E%D1%89%D0%B0%D1%8F-%D0%B8%D0%BA%D0%BE%D0%BD%D0%BA%D0%B0-%D0%B3%D0%BE%D1%80%D0%BE%D0%B4%D0%B0-%D0%B2-%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%B5-%D0%BB%D0%BE%D0%B3%D0%BE%D1%82%D0%B8%D0%BF.jpg?s=2048x2048&w=is&k=20&c=TBwmZ-lVliMXQ-sHNv_mlhlPo2u9t4vlzy33dcoP70k=");
+            ImageIcon icon = new ImageIcon(iconURL);
+            frame.setIconImage(icon.getImage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         frame.setSize(width, height);
         int x = (screenWidth - width) / 2;
         int y = (screenHeight - height) / 2;
