6fb184808761f5b6d596182b5a9d4697b30b19be
AndriiNez <andreynezen@gmail.com>
2023-08-29 20:19:51 +0200

    початок

--- src/main/java/org/group_3/GameLogic.java
+++ src/main/java/org/group_3/GameLogic.java
@@ -1,15 +1,20 @@
 package org.group_3;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-import static org.group_3.GameWindow.usedCities;
+import java.util.Map;
 
 public class GameLogic {
-
-
+    private static final String ABSOLUTE_PARTF = "./textfolder/City.txt";
+    public int moveCounter = 0;
+    // Створюємо додатковий список для збору відповідей.
+    static List<String> usedCities = new ArrayList<>();
     private List<String> citiesList;
-
 
     public GameLogic() {
         citiesList = cities();
@@ -18,31 +23,49 @@ public class GameLogic {
 
     public String generateComputerResponse(String userInput) {
         char lastLetter = Character.toLowerCase(userInput.charAt(userInput.length() - 1));
-
+        char b = 'ь';
+        if (lastLetter == b) {
+            lastLetter = Character.toLowerCase(userInput.charAt(userInput.length() - 2));
+        }
         for (String city : citiesList) {
-            if (city.toLowerCase().charAt(0) == lastLetter &&!GameWindow.isCityUsed(city)) {
+            if (city.toLowerCase().charAt(0) == lastLetter && !isCityUsed(city)) {
                 citiesList.remove(city);
                 return city;
             }
         }
 
-        return "Комп'ютер не знайшов відповідного міста.";
+        return "здаюсь";
     }
 
 
-
     public boolean isValidCity(String city) {
-        for (String citys:citiesList) {
-            if(citys.equalsIgnoreCase(city)){
-                return true;
+        for (String citys : citiesList) {
+            if (citys.equalsIgnoreCase(city)) {
+                return false;
             }
 
+        }
+        return true;
+    }
+
+    public boolean checkingFirstLastSymbol(String userInput) {
+        char firstLetter = Character.toLowerCase(userInput.charAt(0));
+        String lastAddedCity = usedCities.get(usedCities.size() - 1);
+        char lastLetter = Character.toLowerCase(lastAddedCity.charAt(lastAddedCity.length() - 1));
+        if (firstLetter != lastLetter) {
+            return true;
         }
         return false;
     }
 
-
-
+    public static boolean isCityUsed(String city) {
+        for (String citys : usedCities) {
+            if (citys.equalsIgnoreCase(city)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
     public List<String> cities() {
@@ -72,8 +95,11 @@ public class GameLogic {
 
         return citiesList;
     }
+
     public void clearCollections() {
         citiesList.clear();
+        usedCities.clear();
+        moveCounter = 0;
         usedCities.clear();
     }
 
